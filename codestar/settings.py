2726ba6e4d3a9c73745e628f10cfe65abad4e723
Fatima Aminu <teemamin@yahoo.com>
2021-07-27 10:29:29 +0000

    post detail view and template started

--- codestar/settings.py
+++ codestar/settings.py
@@ -41,9 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage', #this needs to go above staticfiles
+    'cloudinary_storage',  # this needs to go above staticfiles
     'django.contrib.staticfiles',
-    'cloudinary', #and this below staticfiles
+    'cloudinary',  # and this below staticfiles
+    'django_summernote',
     'blog',
 ]
 
@@ -130,11 +131,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
  
-STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
